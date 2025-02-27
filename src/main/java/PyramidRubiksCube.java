71a1ab86ad0631002f1b7adcdb380e94b100ebc8
Magetamer <daniel.chahbour@gmail.com>
2025-02-27 11:16:18 -0800

    In Class work

---
+++ src/main/java/PyramidRubiksCube.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+
+enum Color{Red, Green, Blue, Yellow}
+
+class Face{
+    private Color[] tiles;
+    public Face(Color[] color){
+        tiles = new Color[9];
+        Arrays.fill(tiles, color);
+
+    }
+
+    public Color[] getTiles(){
+        return tiles;
+    }
+}
+public class PyramidRubiksCube {
+    private final Face[] faces;
+
+    public PyramidRubiksCube(Face[] faces){
+        this.faces = faces;
+        InitializeFaces();
+        InitializeEdges();
+    }
+
+    public Face[] getFaces(){
+        return faces;
+    }
+
+    private void InitializeFaces() {
+        Color[] color = new Color[9];
+        for(int i = 0; i < faces().length; i++){
+            faces[i] = new Face(color);
+        }
+    }
+
+
+    private void InitializeEdges() {
+
+        int[][] adjacentFace = {
+
+        }
+
+        int[][] edgeIndices
+    }
+
+    public boolean validateCube(){
+
+    }
+
+}
